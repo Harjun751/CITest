f49fc33cdb2873cf3797739a47f34b9f0cc4bb7a
Harjun751 <harjun751@gmail.com>
2022-01-17 08:07:48 +0800

    updated workflow to test

--- simpCity.py
+++ simpCity.py
@@ -1,7 +1,6 @@
-from random import randint
-# initial commit
-print("Welcome, mayor of Simp City")
-print("----------------------------")
+import pickle
+
+# hi2
 
 
 def init_game():
@@ -22,135 +21,96 @@ def init_game():
     }
     return game_board, building_pool
 
+# UI for in-game menu
+
 
 def game_menu(game_board, building_pool):
-    turn_counter = 1
-    while True:
-        # Print turn and game   board
-        print("\nTurn " + str(turn_counter))
-        print_board(game_board)
+    # implement auto Turn counter
+    print("\nTurn Counter")
 
-        # Get randomised building
-        buildings = randomise_building(building_pool)
+    # implement display game board
+    print("Board")
 
-        # Print options for turn
-        print("1. Build a "+buildings[0])
-        print("2. Build a "+buildings[1])
-        print("3. See remaining buildings")
-        print("4. See current score\n")
-        print("5. Save game")
-        print("0. Exit to main menu")
-        print("Your choice?")
-        option = input()
-
-        turn_counter += 1
-        # Ensure inputted option is valid
-        try:
-            option = int(option)
-            if (option != 1 and option != 2 and option != 3 and option != 4 and option != 5 and option != 0):
-                raise ValueError
-        except ValueError:
-            print("\033[91m{}\033[00m".format("Invalid option!"))
-            continue
-
-        if option == 1 or option==2:
-            column = input("Column :")
-            row = input("Row :")
-            if (option==1):
-                to_be_built = buildings[0]
-            elif (option==2):
-                to_be_built = buildings[1]
-
-            game_board = build(game_board,column,row,to_be_built)
-            building_pool[to_be_built] -= 1
-
-        elif option == 3:
-            continue
-        elif option == 4:
-            continue
-        elif option == 5:
-            continue
-        elif option == 0:
-            print("Returning to main menu...")
+    # in-game menu
+    print("1. Build random building")
+    print("2. Build random building")
+    print("3. See remaining buildings")
+    print("4. See current score")
+    print("\n5. Save game")
+    print("0. Exit to main menu")
+    try:
+        choice = int(input("Your choice? "))
+        if choice == 1:
+            # code to add random building 1
+            return game_menu(game_board, building_pool)
+        elif choice == 2:
+            # code to add random building 2
+            return game_menu(game_board, building_pool)
+        elif choice == 3:
+            # code to see remaining buildings
+            return game_menu(game_board, building_pool)
+        elif choice == 4:
+            # code to see current score
+            return game_menu(game_board, building_pool)
+        elif choice == 5:
+            save_game()
+            return game_menu(game_board, building_pool)
+        elif choice == 0:
+            # code to delete existing game data
             return
-    return
+        else:
+            print("\033[91m{}\033[00m".format("Input options 0-5!"))
+            return game_menu(game_board, building_pool)
+    except ValueError:
+        print("\033[91m{}\033[00m".format("Input options 0-5!"))
+        return game_menu(game_board, building_pool)
+
+
+# Function to save game data
+
+
+def save_game():
+    # getting board data (implement get board function)
+    # to change to game data, populating for function testing purpose
+    board = {1: "6", 2: "5", 3: "f"}
+    pickle_out = open("save.pickle", "wb")
+    pickle.dump(board, pickle_out)
+    pickle_out.close()
+
+
+# CREATED FOR UNIT TEST !!!!!!!!!!!!
+def save_game(board, pool, file):
+    pickle_out = open(file, "wb")
+    content = pickle.dump([board, pool], pickle_out)
+    pickle_out.close()
+    return content
+
+# Function to load game data
 
 
 def load_game():
-    # implementation
+    pickle_in = open("save.pickle", "rb")
+    board = pickle.load(pickle_in)
+    # implement an update function to populate saved board
+    # board = city.updatefunction
+
+    # testing whether function works
+    print(board)
     return
 
-
-def randomise_building(building_pool):
-    building_1 = None
-    building_2 = None
-
-    total_buildings = 0
-    for key in building_pool:
-        total_buildings += building_pool[key]
-
-    # Convert building pool and values to list
-    building_categories = list(building_pool.keys())
-    building_values = list(building_pool.values())
-
-    if total_buildings == 0:
-        # No buildings can be built
-        pass
-    elif total_buildings < 2:
-        # Only one building can be built
-        #  Obtain building category with only one building left
-        building_1 = building_categories[building_values.index(1)]
-    else:
-        # Randomise and get 2 buildings
-        while True:
-            index = randint(0, 4)
-            if building_values[index] > 0:
-                # Decrement amount of building category
-                building_values[index] -= 1
-                # Set building for building 1/2
-                if (building_1 == None):
-                    building_1 = building_categories[index]
-                elif (building_2 == None):
-                    building_2 = building_categories[index]
-                    break
-    return [building_1, building_2]
-
-
-def print_board(board):
-    print(f"    {'A':<6}{'B':<6}{'C':<6}{'D':<6}")
-    row_count = 1
-    for row in board:
-        print(" +-----+-----+-----+-----+")
-        #  Prints out contents of row center aligned and with a width of 5
-        print(f"{row_count}|{row[0]:^5}|{row[1]:^5}|{row[2]:^5}|{row[3]:^5}|")
-        row_count += 1
-    print(" +-----+-----+-----+-----+")
-    return
-
-def build(board,column,row,building):
-    # Obtains index from  letter by getting unicode value of letter
-    column_index = ord(column.lower()) - 97
-    row_index = int(row) - 1
-
-    # Set building in board
-    
-    if (board[column_index][row_index]==""):
-        board[column_index][row_index] = building
-    else:
-        raise ValueError("Invalid placement")
-
-    return board
 
 def main():
     game_board = None
     building_pool = None
 
     while True:
+        print("\nWelcome, mayor of Simp City!")
+        print("----------------------------")
         print("\n1. Start new game")
         print("2. Load new game")
         print("\n0. Exit")
 
-        option = input("Your choice? :")
+        option = input("Your choice? ")
 
         # Ensure inputted option is valid
         try:
@@ -179,3 +139,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
